d4205ef77b80293be594cab9738b949db2493520
Nanbon675 <167584843+Nanbon675@users.noreply.github.com>
2025-02-15 03:03:29 +0300

    Add files via upload

---
+++ public/js/firebase.js
@@ -0,0 +1,12 @@
+let firebaseConfig = {
+    apiKey: "AIzaSyB-yuQ99D4i9vM8hj3ESrsgT1KvWR358CQ",
+  authDomain: "bloging-website-60848.firebaseapp.com",
+  projectId: "bloging-website-60848",
+  storageBucket: "bloging-website-60848.firebasestorage.app",
+  messagingSenderId: "985969910903",
+  appId: "1:985969910903:web:e06066b330c35f32df1272"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
